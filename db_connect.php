e245bc6e7227005838d27749bfc44638549af9d8
Faculty of Data Science and Computing (FSDK) <141599942+fsdkumk@users.noreply.github.com>
2025-05-07 12:47:26 +0800

    Update db_connect.php
    Add port

--- db_connect.php
+++ db_connect.php
@@ -3,8 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "fsdk";
+$port = 3306;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
